92fb091bd8b6df79cfe4c5ab7e56d9da29ffe24c
kim yuil <kim yuil>
2021-05-14 18:24:36 +0900

    stack 구현, 오작동 수정

--- calculator/js/main.js
+++ calculator/js/main.js
@@ -2,15 +2,69 @@ const numButtons = document.getElementsByClassName("num");
 const opButtons = document.getElementsByClassName("operator");
 const operateButton = document.getElementById("operate");
 
-var numbers = [];
-var operators = [];
-var expression = [];
+class StackExp {
+  constructor() {
+    this.arr = [];
+    this.index = 0;
+  }
+  push(item) {
+    this.arr[this.index++] = item;
+  }
+  pop() {
+    if (this.index <= 0) return null;
+    const result = this.arr[--this.index];
+    return result;
+  }
+  peek(){
+    return this.arr[this.index-1]
+  }
+  toString(){
+    let finalExpression = "";
+    for (let i = 0; i < this.index; i++) {
+      finalExpression += this.arr[i];
+    }
+    return finalExpression;
+  }
+  length(){
+    return this.index
+  }
+  clean(){
+    this.arr=[]
+    this.index=0
+  }
+  info(){
+    console.log(this.arr)
+    console.log(this.index)
+  }
+  isPuttingFloat(){
+    if(this.length()==1 || isOperator(this.peek())){
+      return false
+    }else{
+      let tempArr = this.arr.slice()
+      let data = []
+      for(let i = this.index-1 ; i >= 0 ; i --){
+        let temp = tempArr.pop()
+        if(isOperator(temp)){
+          break;
+        }else{
+          data.push(temp)
+        }
+      }
+      if(data.indexOf(".")==-1){
+        return false
+      }else{
+        return true
+      }      
+    }
+  }
+}
+var expression = new StackExp();
 
 Object.values(numButtons).forEach((btn) => {
   let val = btn.dataset.num;
   btn.addEventListener("click", () => {
     // putNum(val);
-    putNum2(val);
+    putNum(val);
   });
 });
 
@@ -23,27 +77,44 @@ Object.values(opButtons).forEach((btn) => {
 
 operateButton.addEventListener("click", () => {
   let outputBox = document.getElementsByClassName("output");
-  let result = eval(makeExpression());
+  let result = eval(expression.toString());
   if (isInt(result)) {
     outputBox[0].innerHTML = result;
   } else {
-    outputBox[0].innerHTML = parseFloat(result).toFixed(5);
+    let tmpResult = String(parseFloat(result).toFixed(5))
+    let complete 
+    while(true){
+      if(tmpResult.substring(tmpResult.length-1,tmpResult.length) == '0'){
+        tmpResult = tmpResult.slice(0,tmpResult.length-1)
+      }else{
+        complete = tmpResult
+        break;
+      }
+    }    
+    outputBox[0].innerHTML = complete;
   }
 });
 
-function putNum2(num) {
-  if (expression[expression.length - 1] == "." && num == ".") {
-  } else if (expression[expression.length - 1] == "0" && num == "0") {
+function putNum(num) {
+  if (expression.peek() == "." && num == ".") {
+
+  } else if (expression.peek() == "0" && num == "0" && !expression.isPuttingFloat()) {
+
   } else if (
-    expression[expression.length - 1] == "0" &&
+    expression.peek() == "0" &&
     Number(num) > 0 &&
-    Number(num) < 10
+    Number(num) < 10 &&
+    !expression.isPuttingFloat()
   ) {
-    expression[expression.length - 1] = num;
-  } else if (expression[expression.length - 1] == "-" && num == ".") {
+    //error (float)
+    expression.pop()
+    expression.push(num)
+  } else if (expression.peek() == "-" && num == ".") {
     expression.push("0");
     expression.push(".");
-  } else {
+  } else if (expression.isPuttingFloat() && num == "."){
+
+  }else {
     expression.push(num);
   }
 
@@ -51,11 +122,15 @@ function putNum2(num) {
 }
 
 function putOp(op) {
-  if (op == "delete") {
-    expression = [];
+  if (op == "clear") {
+    expression.clean();
     document.getElementsByClassName("output")[0].innerHTML = "";
-  } else if (isOperator(expression[expression.length - 1]) && isOperator(op)) {
-    expression[expression.length - 1] = op;
+  } else if(op == "del"){
+    expression.pop()
+  }
+  else if (isOperator(expression.peek()) && isOperator(op)) {
+    expression.pop()
+    expression.push(op)
   } else if (op == "bracket") {
     alert("sorry");
   } else {
@@ -63,6 +138,11 @@ function putOp(op) {
   }
 
   drawExpression();
+}
+
+function drawExpression() {
+  let inputBox = document.getElementsByClassName("inputLine");
+  inputBox[0].innerHTML = expression.toString();
 }
 
 function isOperator(check) {
@@ -76,48 +156,3 @@ function isOperator(check) {
 function isInt(n) {
   return parseInt(n) === n;
 }
-
-function drawExpression() {
-  let inputBox = document.getElementsByClassName("inputLine");
-  inputBox[0].innerHTML = makeExpression();
-}
-
-function makeExpression() {
-  let finalExpression = "";
-  for (i = 0; i < expression.length; i++) {
-    finalExpression += expression[i];
-  }
-  return finalExpression;
-}
-
-// function putNum(num) {
-//   if (operators.length == numbers.length) {
-//     if (num == ".") {
-//       numbers.push("0.");
-//     } else {
-//       numbers.push(String(num));
-//     }
-//   } else if (operators.length < numbers.length) {
-//     //숫자 계속 입력중
-
-//     let isFloat = String(numbers[numbers.length - 1]).indexOf(".");
-//     if (isFloat == -1) {
-//       //현재까지 소숫점 없음
-//       if (numbers[numbers.length - 1] == "0" && num != ".") {
-//         numbers[numbers.length - 1] = num;
-//       } else if (num == ".") {
-//         numbers[numbers.length - 1] = String(numbers[numbers.length - 1]) + num;
-//       } else if (numbers[numbers.length - 1] != "0") {
-//         numbers[numbers.length - 1] = String(
-//           Number(numbers[numbers.length - 1]) * 10 + Number(num)
-//         );
-//       }
-//     } else {
-//       // 소수가 입력중
-//       if (num == ".") {
-//         return;
-//       }
-//       numbers[numbers.length - 1] = String(numbers[numbers.length - 1]) + num;
-//     }
-//   }
-// }
