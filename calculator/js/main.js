db7a91d03b0d0bd27497b51fbbc30fd88e0d90ad
kim yuil <kim yuil>
2022-02-28 15:30:10 +0900

    eval() -> stack 계산

--- calculator/js/main.js
+++ calculator/js/main.js
@@ -2,7 +2,7 @@ const numButtons = document.getElementsByClassName("num");
 const opButtons = document.getElementsByClassName("operator");
 const operateButton = document.getElementById("operate");
 
-class StackExp {
+class StackYuil {
   constructor() {
     this.arr = [];
     this.index = 0;
@@ -17,6 +17,9 @@ class StackExp {
   }
   peek(){
     return this.arr[this.index-1]
+  }
+  get(i){
+    return this.arr[i]
   }
   toString(){
     let finalExpression = "";
@@ -58,7 +61,7 @@ class StackExp {
     }
   }
 }
-var expression = new StackExp();
+var expression = new StackYuil();
 
 Object.values(numButtons).forEach((btn) => {
   let val = btn.dataset.num;
@@ -75,24 +78,115 @@ Object.values(opButtons).forEach((btn) => {
   });
 });
 
-operateButton.addEventListener("click", () => {
-  let outputBox = document.getElementsByClassName("output");
-  let result = eval(expression.toString());
-  if (isInt(result)) {
-    outputBox[0].innerHTML = result;
-  } else {
-    let tmpResult = String(parseFloat(result).toFixed(15))
-    let complete 
-    while(true){
-      if(tmpResult.substring(tmpResult.length-1,tmpResult.length) == '0'){
-        tmpResult = tmpResult.slice(0,tmpResult.length-1)
-      }else{
-        complete = tmpResult
-        break;
+operateButton.addEventListener("click", () => {  
+  let inputBox = document.getElementsByClassName("inputLine");
+
+  let operatorStack = new StackYuil();
+  let convertStack = new StackYuil();
+  let calculateStack = new StackYuil();  
+  let temp = "";
+
+  console.log(inputBox[0].innerHTML)
+
+  const f = inputBox[0].innerHTML; //식
+
+  console.log("start!!!!!")
+  for(let i = 0; i<f.length; i++) {
+    console.log(operatorStack.toString(), " / " ,convertStack.toString())
+      const char = f.charAt(i);
+      
+      switch(char) {
+          case '(' :                
+              operatorStack.push(char);
+              break;
+
+          case '+' : case '-' : case '×' : case '/' :
+              while(operatorStack.peek() != null &&
+                  prec(char) <= prec(operatorStack.peek()) ){
+                    convertStack.push(operatorStack.pop());
+              } 
+              operatorStack.push(char);
+              break;
+
+          case ')' :
+            
+            let loop = true;
+            while(loop){
+              let returned_op = operatorStack.pop();
+              if( returned_op === "("){
+                loop = false;
+                continue;
+              }
+              convertStack.push(returned_op)
+            }                
+            break;
+
+          default :
+            temp += char; // isNaN(f.charAt(i+1)) : 다음에 연산자가 나오면
+            if(isNaN(f.charAt(i+1)) || ( (i+1) == f.length )){
+              if(f.charAt(i+1) == '.'){   //소수점처리
+                continue;
+              } else {
+                convertStack.push(temp);
+                temp="";
+              }
+            }
+            break;
       }
-    }    
-    outputBox[0].innerHTML = complete;
+      
   }
+    
+  while(operatorStack.peek() != null){
+    convertStack.push(operatorStack.pop());
+  }    
+  console.log(convertStack.toString())
+
+  for(let i = 0 ; i < convertStack.length() ; i++){
+    if(!isNaN(convertStack.get(i))){
+      //숫자일때
+      calculateStack.push(convertStack.get(i))
+    }else{
+      //연산자일때
+      const b = parseFloat(calculateStack.pop());
+        const a = parseFloat(calculateStack.pop());
+
+        switch(convertStack.get(i)){ // 계산해서 다시 stack에 넣기
+          case '+' : calculateStack.push(a+b);
+            break;
+          
+          case '-' : calculateStack.push(a-b);
+            break;
+
+          case '×' : calculateStack.push(a*b);
+            break;
+
+          case '/' : calculateStack.push(a/b);
+            break;
+        }
+      }
+    }
+  
+    document.getElementsByClassName("output")[0].innerHTML = calculateStack.pop()
+
+  
+    // 꼼수의 방법
+
+  // let result = eval(expression.toString());
+  // if (isInt(result)) {
+  //   outputBox[0].innerHTML = result;
+  // } else {
+  //   let tmpResult = String(parseFloat(result).toFixed(15))
+  //   let complete 
+  //   while(true){
+  //     if(tmpResult.substring(tmpResult.length-1,tmpResult.length) == '0'){
+  //       tmpResult = tmpResult.slice(0,tmpResult.length-1)
+  //     }else{
+  //       complete = tmpResult
+  //       break;
+  //     }
+  //   }    
+  //   outputBox[0].innerHTML = complete;
+  // }
 });
 
 function putNum(num) {
@@ -131,8 +225,6 @@ function putOp(op) {
   else if (isOperator(expression.peek()) && isOperator(op)) {
     expression.pop()
     expression.push(op)
-  } else if (op == "bracket") {
-    alert("sorry");
   } else {
     expression.push(op);
   }
@@ -155,4 +247,21 @@ function isOperator(check) {
 
 function isInt(n) {
   return parseInt(n) === n;
+}
+
+// new ----------- 2022 palm ------------------
+
+function prec(op) {
+  switch(op) {
+      case '(':
+      case ')':
+          return 0;
+      case '+':
+      case '-':
+          return 1;
+      case '×':
+      case '/':
+          return 2;    
+      }
+      return 999;
 }
